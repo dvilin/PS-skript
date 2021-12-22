c58c858ac7a6ed0ae7ccb248d94f1b0cc920ee6b
dvilin <2jzxgt@gmail.com>
2021-12-22 15:25:14 +0300

    up

---
+++ ad_dissmised.ps1
@@ -0,0 +1,15 @@
+﻿$objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+$objSearcher.SearchRoot = "LDAP://ou=Dismissed,ou=Users,ou=CC-Rostov,ou=Branches,dc=corp,dc=tele2,dc=ru"
+$objSearcher.Filter = "(&(objectCategory=person))"
+$users = $objSearcher.FindAll()
+# Количество учетных записей
+$users.Count 
+$users | ForEach-Object {
+   $user = $_.Properties
+   New-Object PsObject -Property @{
+   givenName = [string]$user.givenName
+   sn = [string]$user.sn
+   displayName = [string]$user.displayName
+   description = [string]$user.description
+    }
+} | Export-Csv -NoClobber -Encoding utf8 -Path  c:\dilin\diss.csv
---
+++ logon.ps1
@@ -0,0 +1,6 @@
+﻿cls
+$pc = hostname 
+$date = Get-Date 
+$user = whoami
+$v = $pc +";"+$user+";"+$date
+out-file  -Append \\t2ru\ZRfolders\Scripts\GPOscripts\logon.txt -InputObject $v
